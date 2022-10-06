d4ccbf6f846aebbd77f30b9aa71affa40bfc2c4b
HanningtoneM <hanningtonepompyy@gmail.com>
2022-10-06 17:33:00 +0300

    Remove unnecessary migrations

--- db/migrate/20220906083020_remove_food_user_foreign_key.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFoodUserForeignKey < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :foods, :users
-  end
-end
--- db/migrate/20220907075400_remove_column_quantity_in_food_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnQuantityInFoodTable < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :foods , :quantity
-  end
-end
