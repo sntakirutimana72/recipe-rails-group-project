615384da0e807cb6a3c9e8ed1ade447bd3f8a2ea
HanningtoneM <hanningtonepompyy@gmail.com>
2022-10-06 16:57:15 +0300

    Add price of missing items in shopping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -4,6 +4,8 @@ class ShoppingListsController < ApplicationController
     mr_ids = RecipeFood.where.not('food_id not in (?)', Array.wrap(og_ids)).ids
     found_ids = Set.new(og.ids) - Set.new(mr_ids)
 
-    
+    @counter = og.length
+    @price = og.sum(:price)
+    @foods = Food.where(id: found_ids)
   end
 end
