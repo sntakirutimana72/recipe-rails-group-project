57bbc34f1b88c5096316ca34ca50e8987f59ec68
Steve <sntakirutimana72@gmail.com>
2022-10-06 12:42:51 +0200

    Re-adjust query

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -2,7 +2,7 @@ module RecipeHelper
   def recipe_cost(recipe)
     Food
       .joins(:recipe_foods)
-      .where("recipe_foods.food_id = #{recipe.id}")
+      .where("recipe_foods.recipe_id = #{recipe.id}")
       .sum(:price)
   end
 end
